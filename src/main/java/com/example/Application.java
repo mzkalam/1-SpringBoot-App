81cbe424ca0a2f625ef287ed5bffe0bf2803e213
Moiz <mzkalam01@gmail.com>
2022-09-27 18:31:41 +0530

    We have fixed code

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -9,6 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		//Hi we are going to change some code
 		System.out.println("We have changed now code , plese check and let me know ");
+		System.out.println("We are working in stash mode , once it will complete then only we will continue");
 		SpringApplication.run(Application.class, args);
 	}
 
