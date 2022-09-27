933a7a86880a402620f72c79a7f1715a4f426f32
Moiz <mzkalam01@gmail.com>
2022-09-27 17:59:43 +0530

    Main class modified

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		//Hi we are going to change some code
+		System.out.println("We have changed now code , plese check and let me know ");
 		SpringApplication.run(Application.class, args);
 	}
 
